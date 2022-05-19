c95a84e5444e79c29c31a9290cddaa1e5a529655
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-05-19 17:20:08 +0530

    Create 06.Display Array.java

---
+++ Level 1/Recursion And Backtracking/06.Display Array.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        displayArr(arr,0);
+    }
+
+    public static void displayArr(int[] arr, int idx){
+        if(idx==arr.length){
+            return ;
+        }
+        System.out.println(arr[idx]);
+        displayArr(arr,idx+1);
+
+    }
+
+}
