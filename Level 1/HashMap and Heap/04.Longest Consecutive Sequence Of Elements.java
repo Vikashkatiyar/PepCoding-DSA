3d2482130bf7ad6cd133355258a311b8be3f38ab
arpit5220 <arpitkatiyar09197@gmail.com>
2022-10-10 22:10:29 +0530

    I write another approach t solve this problem

--- Level 1/HashMap and Heap/04.Longest Consecutive Sequence Of Elements.java
+++ Level 1/HashMap and Heap/04.Longest Consecutive Sequence Of Elements.java
@@ -49,3 +49,24 @@ public static void main(String[] args) throws Exception {
  }
 
 }
+
+/* without using of HashMap we solve it 
+// this is a better approach for this problem in 0(1) space
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0||nums.length==1)
+        return nums.length;
+
+        Arrays.sort(nums);
+
+        int max=Integer.MIN_VALUE,c=1;
+
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i+1]-nums[i]==1) c++;
+            else c=nums[i+1]==nums[i]?c:1;
+            if(c>max) max=c;
+        }
+
+        return max<0?0:max;
+    }
+} */
