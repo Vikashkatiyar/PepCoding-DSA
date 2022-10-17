4c070c257f9d3b62731302505718e0cd703ee2ac
anuj katiyar <106858176+AnujKatiyar661@users.noreply.github.com>
2022-10-17 22:36:56 +0530

    Update Quickselect.java
    Added my code of Quick select

--- Level 1/Searching and sorting/Quickselect.java
+++ Level 1/Searching and sorting/Quickselect.java
@@ -47,6 +47,9 @@ public class Main {
     System.out.println();
   }
 
+  
+  
+  
   public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
@@ -58,4 +61,38 @@ public class Main {
     System.out.println(quickSelect(arr,0,arr.length - 1,k - 1));
   }
 
+}
+
+
+
+
+
+//
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int start = 0, end = nums.length - 1, index = nums.length - k;
+        while (start < end) {
+            int pivot = partion(nums, start, end);
+            if (pivot < index) start = pivot + 1; 
+            else if (pivot > index) end = pivot - 1;
+            else return nums[pivot];
+        }
+        return nums[start];
+    }
+    
+    private int partion(int[] nums, int start, int end) {
+        int pivot = start, temp;
+        while (start <= end) {
+            while (start <= end && nums[start] <= nums[pivot]) start++;
+            while (start <= end && nums[end] > nums[pivot]) end--;
+            if (start > end) break;
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+        }
+        temp = nums[end];
+        nums[end] = nums[pivot];
+        nums[pivot] = temp;
+        return end;
+    }
 }
