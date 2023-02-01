a44eacb5f1b48752c344d52a2aa5b785dc4c2dd5
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-02-01 14:37:21 +0530

    Create 08.Combinations - 2.java

---
+++ Level 2/Recursion and Backtraking/08.Combinations - 2.java
@@ -0,0 +1,36 @@
+//item on level
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void combinations(int[] boxes, int ci, int ti, int lb){
+      if(ci>ti){
+        for(int i=0;i<boxes.length;i++){
+          if(boxes[i]>0)
+             System.out.print("i");
+          else
+             System.out.print("-");  
+        }
+        System.out.println();
+        return;
+      }
+
+      for(int i=lb+1;i<boxes.length;i++){
+        if(boxes[i]==0){
+          boxes[i]=ci;
+          combinations(boxes,ci+1,ti,i);
+          boxes[i]=0;
+        }
+      }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nboxes = Integer.parseInt(br.readLine());
+    int ritems = Integer.parseInt(br.readLine());
+    combinations(new int[nboxes], 1, ritems, -1);
+  }
+
+}
